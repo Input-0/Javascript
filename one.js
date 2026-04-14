c371b46b86b042a1469c56bedf236762c3db93a8
input-0 <tyogi2015@gmail.com>
2026-04-14 15:42:34 +0530

    first lacture

---
+++ one.js
@@ -0,0 +1,47 @@
+// let first = "one" //string
+// let num = 12 //Number
+// let bigInt = 123123123n //bigInt
+// let bool = false //boolean
+// let b //undefined
+// let c = null 
+// let seven = Symbol("one")
+// let eight = Symbol("one")
+
+// console.log(seven==eight);
+
+// let a = 10
+
+// let b = a  //10//11
+// b=11
+// a = b
+// console.log(a);
+
+
+// let arr = [12,13,14,15]
+
+// let b = arr
+// b[0] = "onetwothree"
+// console.log(arr);
+
+// object letrels
+
+let ob = {
+    firstName : "tinashe",
+    lastname:"john",
+    run:function(){
+        return "tiashe is running"
+        
+    }
+
+}
+console.log(ob.run());
+
+
+
+
+
+
+
+
+
+
