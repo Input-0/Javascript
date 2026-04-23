6fa908b215d8ca02964c9d38a2868d78fa87686f
Input-0 <tyogi2015@gmail.com>
2026-04-23 15:26:11 +0530

    object

---
+++ 2-30/one.js
@@ -0,0 +1,77 @@
+// let arr = [1,2,3,4]
+
+// let ar = arr.reduce((acc,curr)=>{
+//      acc.push(curr)
+//      return acc
+
+// },[])
+// console.log(ar)
+
+// let a = [1,2,3,4,5,6]
+
+// a.forEach((val)=>{
+//     console.log(val+2);
+    
+
+// })
+
+// let a = [1,2,3,4,5,6]
+
+// for(let val in a){
+//     console.log(val);
+    
+// }
+
+
+// Object ------------------------------------------------------>
+
+let ob = { //template letrals
+    name:"tinashe",
+    gender:"male",
+    "current age":21,
+    // run:function(data){
+    //     console.log( this.name,"is running" );
+        
+
+    // }
+
+    run(){
+        console.log( this.name,"is running" );
+        
+
+    }
+
+
+} 
+// ob.run("tinashe")
+// ob.run()
+// function inside an obj is called method
+
+
+
+// ob.age = 22
+// ob.role = "devloper"
+// delete ob.age
+// ob.age= 23
+
+
+// console.log(ob["current age"]); // bracket notation 
+
+// console.log(Object.entries(ob));
+// console.log(Object.keys(ob));
+// console.log(Object.values(ob));
+
+// Object.freeze(ob)
+
+// ob.gender = "female"
+// delete ob["current age"]
+
+// Object.seal(ob)
+
+//  ob.fun = "one"
+
+// console.log(ob);
+
+
+
+
