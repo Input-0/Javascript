450853cf1cb3f37423f473e841763fb119bc2a5e
Input-0 <tyogi2015@gmail.com>
2026-04-30 15:42:57 +0530

    functions in js

---
+++ 2-30/function.js
@@ -0,0 +1,105 @@
+// function one(a,b,...c){
+//     // console.log(a,b);
+//     // return a+b+c[0]+c[1]+c[2]
+    
+//     // console.log(a,b,c);
+    
+    
+// }
+
+// // let sum = one(12,14)
+// // console.log(sum);
+
+// let sum = one(12,14,24,45,67,32)
+// console.log(sum);
+
+
+
+
+//  higher order function -two, callfunction ----one
+
+
+// function one(val1,val2){
+//     return val1+val2
+    
+    
+    
+// }
+
+
+
+// function two(addingfun){
+//     let a = 10
+//     let b = 20
+//     return addingfun(a,b)
+    
+    
+// }
+
+// two(one)
+
+
+// function two(addingfun){
+//     let a = 10
+//     let b = 20
+//      function one(){
+//     return a+b
+// }
+
+// return one()
+    
+    
+// }
+
+// console.log(two());
+
+
+
+// pure function and impure function 
+// pure
+// function one(a,b,c){
+//     console.log(a+b+c);
+// }
+
+
+// one(1,2,3)
+
+// impure function 
+// let b = 29
+// function two(a){
+//     console.log(a+b);
+    
+
+// }
+
+// two(12)
+
+// closure *****
+
+
+// function outer () {
+//     let count = 1
+//     function inner(){
+//         count++
+//         console.log(count);
+        
+//     }
+//     return inner
+// }
+
+// let a = outer()
+// a()
+// a()
+// a()
+
+
+
+
+
+
+
+
+
+
+
+
