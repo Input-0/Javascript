662c88f88bce6e8b935a84ec57a9c5bf5dbf915d
Input-0 <tyogi2015@gmail.com>
2026-05-07 15:44:11 +0530

    dom day2

--- 2-30/dom/dom.js
+++ 2-30/dom/dom.js
@@ -1,16 +1,69 @@
 let header = document.getElementById("top")
-let contain = document.getElementsByClassName("container")
-let btn = document.getElementsByTagName("button")
-let Tname = document.getElementsByName("navigation")
+let contain = document.getElementsByName("container")[0]
+// let btn = document.getElementsByTagName("button")
+// let Tname = document.getElementsByName("navigation")
+// header.className = "first"
+// header.className += " bottom top bottom"
+// header.className += " first top bottom"
+
+// header.classList.add("top")
+// header.classList.add("bottom")
+// // header.classList.add("top bottom")
+// header.classList.remove("top")
+
+// header.classList.toggle("bottom")
+
+// header.setAttribute("style","background-color:green;")
+let id = header.getAttribute("id")
+
+// header.style.cssText = "background-color:green;color:white; padding:20px"
+
+// let btn1 = document.querySelector("button")
+// let btn3 = document.querySelector("button")
+
+// let one = contain.parentElement.lastElementChild
+// console.log(one.textContent);
+
+// firstElementChild.nextElementSibling.previousElementSibling
 
 
-let btn1 = document.querySelector("button")
-let btn3 = document.querySelectorAll("button")
+console.log(id);
+
 
 
 
 // console.log(header.textContent);
 // console.log(header.innerText);
 // console.log(header.innerHTML);
+// console.log(contain.outerHTML);
 
-console.log(header.outerHTML);
+
+// btn3.innerText = "<h1>its h1</h1> "
+
+
+
+
+// console.log(contain)
+// contain.forEach((val) =>{
+//     let newbtn = document.createElement("button")
+// newbtn.textContent = "Login buttton"
+// let newbtn2 = document.createElement("button")
+// newbtn2.textContent = "register buttton"
+//     val.insertAdjacentElement("afterend",newbtn)
+//     val.insertAdjacentText("afterend","here we go")
+//     val.insertAdjacentHTML("afterend","<h1>yes its heading </h1>")
+
+// })
+
+
+
+
+
+
+
+
+
+
+// console.log(btn3);
+
+
