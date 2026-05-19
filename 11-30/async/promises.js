87d4ffbdedbe6a15381e867ba21d969c4831fcc0
Input-0 <tyogi2015@gmail.com>
2026-05-19 13:32:09 +0530

    async await

--- 11-30/async/promises.js
+++ 11-30/async/promises.js
@@ -1,17 +1,75 @@
+// function one() {
+//     return new Promise((resolve,reject) =>{
+//             setTimeout(()=>{
+//             console.log("first");
+//             resolve()
+            
+
+//         },5000)
+//         })
+
+// }
+// function two() {
+//     return new Promise((resolve,reject) =>{
+//             setTimeout(()=>{
+//             console.log("second");
+//             resolve()
+            
+
+//         },4000)
+//         })
+
+// }
+// function three() {
+//     return new Promise((resolve,reject) =>{
+//             setTimeout(()=>{
+//             console.log("third");
+            
+//             resolve()
+
+//         },2000)
+//         })
+
+// }
+// function four() {
+//     return new Promise((resolve,reject) =>{
+//             setTimeout(()=>{
+//             console.log("four");
+            
+
+//         },1000)
+//         })
+
+// }
+// one().then(() =>two().then(() => three().then(() => four())))
 
 
-let one = new Promise((resolve,reject) => {
-    let dbReq = false
 
-    if(dbReq){
-        resolve({user:"one",age:22})
-    }else{
-        reject(new Error("DB connection failed"))
-    }
-});
+// Promise.all()
+// Promise.race()
+// Promise.allSettled()
+// Promise.any()
 
-one
-.then((data) => console.log(data))
-.catch((err) => console.log(err))
-.finally(() => console.log("promise complete"))
 
+
+    // fetch("https:typicode.com/posts")
+    // .then((data) => data.json())
+    // .then((data) => console.log(data))
+    // .catch((err) => console.log(err))
+
+    // async function one (){
+    //   try {
+    //     let response = await  fetch("https:typicode.com/posts")
+    //     let data = await response.json()
+    //     console.log(data);
+    //   } catch (error) {
+    //     console.log(error);
+        
+        
+    //   }
+      
+
+    // }
+    // one()
+    // console.log("first");
+    
